6242a7c425c549e27494818db8b539d107fba594
superz3 <zhangzz1024@163.com>
2023-08-25 18:24:32 +0800

    feat env

---
+++ example/App.js
@@ -0,0 +1,21 @@
+import Button from '@rnmu/components/src/Button';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Button/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
