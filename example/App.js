ab67cdfeb2705cc8508171be08c5c3aa5ba74e64
superz3 <zhangzz1024@163.com>
2023-08-29 18:15:39 +0800

    feat env

--- example/App.js
+++ example/App.js
@@ -1,9 +1,12 @@
+import { Button } from '@rnmu/theme';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Button/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
