57dbb63e0475eaf1e0dbd7214f2faa3e1bf284de
superz3 <zhangzz1024@163.com>
2024-08-15 11:51:37 +0800

    fix: remove unnecessary docusaurus module in .gitignore

--- docs/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/neo/Desktop/product/rnmu/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/neo/Desktop/product/rnmu/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/neo/Desktop/product/rnmu/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/neo/Desktop/product/rnmu/docs/src/css/custom.css"),
-  require("/Users/neo/Desktop/product/rnmu/docs/plugins/snackPlayerInitializer.js"),
-];
